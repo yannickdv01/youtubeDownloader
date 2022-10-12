364d4e8f2ebc7d6f10c47373a40b9ef38545954f
Yannick <yannickvreede@gmail.com>
2022-10-12 14:07:08 +0200

    fixed resolutions

--- Backend/VideoDownloader.py
+++ Backend/VideoDownloader.py
@@ -22,14 +22,14 @@ def DisplayVideoInfo(video : YouTube):
     print("Views: " + video.views)
     print("Length: " + video.length)
 
-def Youtube_Downloader(video : YouTube, format : str):
+def Youtube_Downloader(video : YouTube, format : str, resolution : str = ""):
     try:
-        #object creation using YouTube which was #imported in the beginning
-        get = video.streams.filter(progressive=True, file_extension=format).get_highest_resolution()
-
+        
+            
         print(Fore.GREEN + "Downloading...")
         #downloading the video
-        get.download()
+        #get.download()
+        video.streams.filter(progressive=True, file_extension=format, res=resolution).first().download()
         print("Video downloaded, you will be sent back to the homepage in a few seconds :)" + Style.RESET_ALL)
 
         time.sleep(2)
--- Frontend/Downloadpage.py
+++ Frontend/Downloadpage.py
@@ -1,3 +1,4 @@
+from ast import And
 import imp
 import sys
 from os.path import dirname, abspath
@@ -28,22 +29,45 @@ def DownLoadSubPage():
         ]
     ).display()
 
-def FormatSelection(video):
+def FormatSelection(video: YouTube):
+    #print all resolutions
     Menu.SelectionMenu("Select the format you want to download", 
         [
-            Menu.MenuOption("Video", lambda: VideoDownloader.Youtube_Downloader(video, "mp4")),
+            Menu.MenuOption("Video", lambda: ResolutionSelection(video)),
             Menu.MenuOption("Audio", lambda: VideoDownloader.Youtube_Downloader(video, "mp3")),
             Menu.MenuOption("Go back", DownloadPage)
         ]
     ).display()
 
-def ResolutionSelection(video):
-    Menu.SelectionMenu("Select the resolution you want to download", 
-        [
-            #TODO add a way to get the resolutions
-        ]
-    ).display()
+#-----------------RESOLUTION SELECTION-----------------#
+def ResolutionSelection(video : YouTube):
+    #print all resolutions
+    for stream in video.streams:
+        print(stream.resolution, stream.type)
 
+    #get all unique resolutions
+    resolutions = []
+    for stream in video.streams:
+        if stream.resolution not in resolutions and stream.resolution != None:
+            resolutions.append(stream.resolution)
+
+    #sort resolutions
+    resolutions.sort(key=lambda x: int(x.split("p")[0]))
+    
+    #create a list of options
+    
+    options = []
+    for resolution in resolutions:
+        options.append(Menu.MenuOption(resolution, lambda: VideoDownloader.Youtube_Downloader(video, "mp4")))
+    #add go back and exit option
+    options.append(Menu.MenuOption("Home", Home.homescreen))
+    options.append(Menu.MenuOption("Go back to download page", DownloadPage))
+    options.append(Menu.MenuOption("Exit", exit))
+    Menu.SelectionMenu("Select the resolution you want to download", 
+        options
+    ).display()
+    
+#-----------------Playlist-----------------#
 def DownloadPlaylist():
     Link = input("Enter the link for the playlist you want to download: ")
     Playlist = VideoDownloader.GetPlaylistInfo(Link)
